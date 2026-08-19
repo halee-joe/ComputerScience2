675c56724d6dd3c64632f17eeee5a0563d3f80ca
Halle Jones <hallejo225@gmail.com>
2026-08-19 17:05:47 -0600

    commit2:)

--- src/CS2day1.java
+++ src/CS2day1.java
@@ -6,8 +6,6 @@ public class CS2day1 {
             total += i;
         }
         System.out.println(total/2);
-        
-       // System.out.println("Problem 2");
         // Problem 2 
         //int[] a = {3,7,2,9};
         //for (int i = 0; i <= a.length; i++) {
@@ -21,38 +19,35 @@ public class CS2day1 {
         //System.out.println(s1 == s2);
         //System.out.println(s1.equals(s2));
 
-        System.out.println("Problem 4 ");
+     // Problem 4 
         int x = 5;
         bump(x);
         System.out.println(x);
-
-        System.out.println("Problem 5");
+        //Problem5 
 
         int[] y = {5};
         bump(y);
         System.out.println(y[0]);
 
-        //System.out.println("Problem 6");
+        //Problem 6
         //String[] names = new String[3];
         //System.out.println(names[0].length());
 
-        //System.out.println("Problem 7");
+        //Problem 7
         ///Counter c = new Counter();
         //c.setCount(10);
         //System.out.println(c.getCount());
 
-        //System.out.println("Problem 8");
-
+        //Problem 8
         //int[] scores = {88, 92, 79};
         //for (int i = 0; i < scores.length; i++){
             //int doubled = scores[i] * 2;
         //}
         //System.out.println(doubled);
 
-
     }
 
-
+    // Problem 7
     public class Counter {
         private int count = 0;
 
@@ -64,11 +59,11 @@ public class CS2day1 {
         }
     }
 
-
+    // Problem 4
     public static void bump(int n){
         n = n + 1; 
     }
-
+    // Priblem 5
     public static void bump(int[] arr){
         arr[0] = arr[0] + 1;
     }
